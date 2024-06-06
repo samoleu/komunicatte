fb91d6dd5fbb25a24d67e8cd8566d38bd294d3bd
Samuel Ferreira <samuel.ferralves@gmail.com>
2024-06-06 16:33:58 -0300

    feat: add  and setting up chatModel

---
+++ api/models/chatModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const ChatSchema = mongoose.Schema(
+    {
+        profilePicture: {
+            type: String,
+            required: false,
+            default: "https://imgur.com/mCHMpLT",
+        },
+        chatName: {
+            type: String,
+            required: true,
+        },
+        lastMessage: {
+            type: String,
+            required: false,
+        },
+        lastMessageTimestamp: {
+            type: Date,
+            required: false,
+        },
+        participants: {
+            type: Array,
+            required: true,
+            default: [],
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Chat = mongoose.model("Chat", ChatSchema);
+
+module.exports = Chat;
