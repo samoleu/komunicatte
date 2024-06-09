92224549f49ece613f2d3ff54af7db6a0e235896
Samuel Ferreira <samuel.ferralves@gmail.com>
2024-06-09 19:23:14 -0300

    style: format documents for pattern

--- api/models/chatModel.js
+++ api/models/chatModel.js
@@ -1,35 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const ChatSchema = mongoose.Schema(
-    {
-        lastMessage: {
-            message: {
-                type: String,
-                required: false,
-            },
-            sender: {
-                type: String,
-                required: false,
-            },
-            timestamp: {
-                type: Date,
-                required: false,
-            },
-            status: { // true: lida, false: não lida
-                type: Boolean,
-                required: false,
-                default: false,
-            },
-        },
-        members: {
-            type: Array,
-            required: true,
-            default: [],
-        },
+  {
+    lastMessage: {
+      message: {
+        type: String,
+        required: false,
+      },
+      sender: {
+        type: String,
+        required: false,
+      },
+      timestamp: {
+        type: Date,
+        required: false,
+      },
+      status: {
+        // true: lida, false: não lida
+        type: Boolean,
+        required: false,
+        default: false,
+      },
     },
-    {
-        timestamps: true,
-    }
+    members: {
+      type: Array,
+      required: true,
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const Chat = mongoose.model("Chat", ChatSchema);
