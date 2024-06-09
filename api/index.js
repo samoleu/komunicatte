044154f67fdd3449bd085ea556a9351ed634e4c5
Jhonata Demuner <jhonata.demuner@gmail.com>
2024-06-09 19:17:28 -0300

    build: use cors to avoid errors in development

--- api/index.js
+++ api/index.js
@@ -1,28 +1,31 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const exampleRoute = require('./routes/exampleRoute');
-const chatRoute = require('./routes/chatRoute');
-const mongoose = require('mongoose');
+const express = require("express");
+var cors = require("cors");
+const exampleRoute = require("./routes/exampleRoute");
+const chatRoute = require("./routes/chatRoute");
+const mongoose = require("mongoose");
+
 const mongoString = process.env.DATABASE_URL;
- 
+
 mongoose.connect(mongoString);
 const database = mongoose.connection;
 
-database.on('error', (error) => {
-    console.log(error)
-})
+database.on("error", (error) => {
+  console.log(error);
+});
 
-database.once('connected', () => {
-    console.log('Database Connected');
-})
+database.once("connected", () => {
+  console.log("Database Connected");
+});
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use('/api/example', exampleRoute);
-app.use('/api/chat', chatRoute);
+app.use("/api/example", exampleRoute);
+app.use("/api/chat", chatRoute);
 
-app.listen(3001, () => {
-    console.log(`Server Started at ${3001}`)
-})
+app.listen(3000, () => {
+  console.log(`Server Started at ${3000}`);
+});
