e9fea23a0163749e753cfea8f30bd2bdd193569b
Jhonata Demuner <jhonata.demuner@gmail.com>
2024-06-09 19:25:51 -0300

    feat: clerk api and komunicatte api request

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import ClerkBtn from './components/ClerkBtn'
-import './App.css'
+import { useState, useEffect } from "react";
+import { useAuth } from "@clerk/clerk-react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import ClerkBtn from "./components/ClerkBtn";
+import "./App.css";
+import axios from "axios";
+
+const apiUrl = import.meta.env.VITE_BASE_URI;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [chats, setChats] = useState([]);
+  const { userId } = useAuth();
+
+  const userChats = async () => {
+    if (userId) {
+      try {
+        const response = await axios.get(`${apiUrl}/api/chat/${userId}`);
+        setChats(response.data);
+      } catch (error) {
+        console.error("Error fetching user chats:", error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    userChats();
+  }, [userId]);
+
+  const testEnv = () => {
+    console.log("BASE_URI:", import.meta.env.VITE_BASE_URI);
+  };
+
+  useEffect(() => {
+    testEnv();
+  }, []);
 
   return (
     <>
-      <ClerkBtn/>
-      <div className='flex justify-center items-center'>
+      <ClerkBtn />
+      <div>Your userId is: {userId}</div>
+      <div>You currently have {chats.length} chats</div>
+      <div className="flex justify-center items-center">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -31,7 +62,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
