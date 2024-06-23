f574c73dd4da98c6fb8704f92fe3fb49c3d30957
João Pedro Spinassé <jpspinasse@gmail.com>
2024-06-23 20:47:13 -0300

    fix: code consistency

--- frontend/src/components/ProfileCard.tsx
+++ frontend/src/components/ProfileCard.tsx
@@ -5,29 +5,33 @@ import Image from "next/image";
 
 interface ProfileCardProps {
   profile: {
+    accountRef: string;
+    profileId: string;
     profileName: string;
-    bio: string;
     activityStatus: string;
+    bio: string;
     profilePicture: string;
+    friends: string[];
+    communityRefs: string[];
   };
 }
 
 const ProfileCard = ({ profile }: ProfileCardProps) => {
-  let activityColor;
+  
+  const getActivityColor = (status: string) => {
+    switch (status.toLowerCase()) {
+      case "online":
+        return "border-onlineActivity";
+      case "offline":
+        return "border-offlineActivity";
+      case "do not disturb":
+        return "border-dndActivity";
+      default:
+        return "border-offlineActivity";
+    }
+  };
 
-  switch (profile.activityStatus.toLowerCase()) {
-    case "online":
-      activityColor = "rgb(0,200,0)";
-      break;
-    case "offline":
-      activityColor = "rgb(128,128,128)";
-      break;
-    case "do not disturb":
-      activityColor = "rgb(255,0,0)";
-      break;
-    default:
-      activityColor = "rgb(0,200,0)";
-  }
+  const activityColor = getActivityColor(profile.activityStatus);
 
   const truncatedName =
     profile.profileName.length > 16
@@ -56,7 +60,7 @@ const ProfileCard = ({ profile }: ProfileCardProps) => {
         </div>
       </div>
       <div className="absolute top-[18.5rem] left-1/2 transform -translate-x-1/2 w-full text-center">
-        <h1 className="text-xl font-bold text-black">{truncatedName}</h1>
+        <p className="text-xl font-bold text-black">{truncatedName}</p>
       </div>
       <div className="absolute top-[21rem] left-1/2 transform -translate-x-1/2 w-[90%] h-24">
         <p className="w-full h-full text-center text-md text-black bg-[rgba(252,252,255,0.3)] p-3 rounded-lg break-words overflow-hidden">
@@ -65,10 +69,8 @@ const ProfileCard = ({ profile }: ProfileCardProps) => {
       </div>
       <div className="absolute bottom-[-4.25rem] left-1/2 transform -translate-x-1/2">
         <div
-          className="w-[8.625rem] h-[8.625rem] rounded-full border-[1rem] flex items-center justify-center cursor-pointer"
-          style={{ borderColor: activityColor }}
-        >
-          <span className="absolute text-lg font-semibold text-black top-[2rem]">
+          className={`w-[8.625rem] h-[8.625rem] rounded-full border-[1rem] flex items-center justify-center cursor-pointer ${activityColor}`}>
+          <span className="absolute text-lg font-semibold text-black top-[2rem] select-none">
             {profile.activityStatus}
           </span>
         </div>
