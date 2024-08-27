a1a3c12fa92e6f709011a323f063db8af580deba
Erick Komati <109813173+kenzokomati@users.noreply.github.com>
2024-08-27 01:20:46 -0300

    feat: authentication check

--- frontend/src/app/app/page.tsx
+++ frontend/src/app/app/page.tsx
@@ -1,12 +1,45 @@
+'use client';
+
 import CommunityMenu from "@/components/CommunityMenu";
 import UserInfo from "@/components/UserInfo";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useUser, useSignIn } from "@clerk/nextjs";
 import Image from "next/image";
+import React, { use } from "react";
 
-export default function Home() {
+const Page = () => {
+  const { isLoaded, isSignedIn } = useUser();
+
+  if (!isLoaded) {
+    return <p>KOMUNICATTE</p>; // or a loading indicator
+  }
+
+  if (!isSignedIn) {
+    // Handle the signed-out state here
+    return <p>Please sign in</p>;
+  }
+
   return (
     <>
-      <CommunityMenu />
+      <div className="flex flex-col items-center h-screen w-screen bg-gradient-to-b from-[#9a94d5] to-[#DDDBFF] bg-opacity-60">
+        <main className="2xl:w-[96rem] xl:w-[80rem] lg:w-[64rem] md:w-[48rem] sm:w-[40rem] w-full h-full flex flex-col items-center">
+          <section className="flex justify-between items-center px-8 pt-4 w-full">
+            <div className="flex items-center space-x-4">
+              <Image
+                src="https://imgur.com/ezguYup"
+                alt="komunicatte-logo"
+                width={50}
+                height={50}
+                className="object-contain"
+              />
+              <h1 className="text-4xl font-bold text-black leading-tight">Komunicatte</h1>
+            </div>
+            <UserButton />
+          </section>
+          <CommunityMenu />
+        </main>
+      </div>
     </>
   );
-}
+};
+
+export default Page;
--- frontend/src/components/SignInBtn.tsx
+++ frontend/src/components/SignInBtn.tsx
@@ -1,15 +1,30 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, useUser } from "@clerk/nextjs";
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 const ClerkBtn = () => {
+  const { isSignedIn, user } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log(user);
+    if (isSignedIn) {
+      router.push('/app'); // Redirect to the /app page when signed in
+    }
+  }, [isSignedIn, router]);
+
   return (
     <>
       <SignedOut>
         <SignInButton mode="modal">
-          <button className="bg-black text-white py-2 px-4 rounded-lg text-lg hover:bg-gray-800">Sign in</button>
+          <button className="bg-black text-white py-2 px-4 rounded-lg text-lg hover:bg-gray-800">
+            Sign in
+          </button>
         </SignInButton>
       </SignedOut>
+
       <SignedIn>
-        <UserButton />
+        <div>-- LOGGED IN --</div>
       </SignedIn>
     </>
   );
