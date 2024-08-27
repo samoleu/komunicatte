f6f94131b753fe85a3d4db859d59ee7f18e58919
Samuel Ferreira <samuel.ferralves@gmail.com>
2024-08-27 18:28:38 -0300

    feat: all.

--- api/routes/profileRoute.js
+++ api/routes/profileRoute.js
@@ -4,15 +4,21 @@ const router = express.Router();
 const {
   getAllProfiles,
   getProfileById,
+  getProfileByName,
   createProfile,
   updateProfile,
   deleteProfile,
+  addFriend,
+  removeFriend
 } = require("../controllers/profileController");
 
 router.get("/", getAllProfiles);
 router.get("/:id", getProfileById);
+router.get("/name/:name", getProfileByName);
 router.post("/", createProfile);
 router.put("/:id", updateProfile);
 router.delete("/:id", deleteProfile);
+router.post("/friend/:id", addFriend);
+router.delete("/friend/:id", removeFriend);
 
 module.exports = router;
